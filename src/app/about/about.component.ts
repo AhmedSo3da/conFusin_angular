101cd22991fdb88bd4b679bc8b1265f4c90fb56c
AhmedSo3da <43776100+AhmedSo3da@users.noreply.github.com>
2020-11-21 01:03:36 +0200

    online json

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,11 +3,19 @@ import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { LeaderService } from '../services/leader.service';
 import { Leader } from '../shared/leader';
+import { flyInOut } from '../animations/app.animations';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+    },
+    animations: [
+      flyInOut()
+    ]
 })
 export class AboutComponent implements OnInit {
 
--- src/app/shared/baseurl.ts
+++ src/app/shared/baseurl.ts
@@ -1,1 +1,2 @@
-export const baseURL = 'http://localhost:3000/';
+//export const baseURL = 'http://localhost:3000/';
+export const baseURL = 'https://my-json-server.typicode.com/Ahmedso3da/fakeServer-angular/';
